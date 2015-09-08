4954e3621246bda52fee207e60ce9029ac8a4fe3
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-09-08 21:06:11 +0000

    Do Python 2.6 tests and test with missing argparse

--- test_flake8_indexed_format.py
+++ test_flake8_indexed_format.py
@@ -5,6 +5,7 @@ import codecs
 import itertools
 import os
 import re
+import sys
 import tempfile
 import unittest
 
@@ -62,44 +63,90 @@ class SimpleImportTestCase(TestCaseBase):
         checker = flake8_indexed_format.UnindexedParameterChecker(tree, 'fn')
         self.run_test_tree(tree, positions, iterator())
 
-    def test_main_invalid(self):
-        self.assertRaises(SystemExit, flake8_indexed_format.main,
-            ['--ignore', 'foobar', '/dev/null'])
 
-
-class TestMainPrintPatched(TestCaseBase):
+class TestPatchedPrint(unittest.TestCase):
 
     def patched_print(self, msg):
-        match = re.match(r'([^:]+):(\d+):(\d+): (.*)', msg)
-        # two header lines and col offset is increased by one
-        self.messages += [(match.group(1), int(match.group(2)) - 2,
-                           int(match.group(3)) - 1, match.group(4))]
+        self.messages += [msg]
 
     def setUp(self):
-        super(TestMainPrintPatched, self).setUp()
+        super(TestPatchedPrint, self).setUp()
         flake8_indexed_format.print = self.patched_print
+        self.messages = []
 
     def tearDown(self):
         flake8_indexed_format.print = print
-        super(TestMainPrintPatched, self).tearDown()
+        super(TestPatchedPrint, self).tearDown()
+
+
+class TestMainPrintPatched(TestPatchedPrint, TestCaseBase):
+
+    def setUp(self):
+        if isinstance(flake8_indexed_format.argparse, ImportError):
+            raise unittest.SkipTest('argparse is not available')
+        super(TestMainPrintPatched, self).setUp()
 
     def iterator(self):
-        for fn, line, char, msg in self.messages:
-            yield line, char, msg
+        for msg in self.messages:
+            match = re.match(r'([^:]+):(\d+):(\d+): (.*)', msg)
+            fn, line, char, msg = match.groups()
+            yield int(line) - 2, int(char) - 1, msg
             self.assertEqual(fn, self.tmp_file)
 
-    def test_main(self):
+    def run_test(self, ignored=''):
         self.messages = []
         code, positions = generate_code()
+        if ignored:
+            positions = []
+            parameters = ['--ignore', ignored]
+        else:
+            parameters = []
         code = '#!/usr/bin/python\n# -*- coding: utf-8 -*-\n' + code
         self.tmp_file = tempfile.mkstemp()[1]
         try:
             with codecs.open(self.tmp_file, 'w', 'utf-8') as f:
                 f.write(code)
-            flake8_indexed_format.main([self.tmp_file])
+            flake8_indexed_format.main(parameters + [self.tmp_file])
         finally:
             os.remove(self.tmp_file)
         self.run_test_code(code, positions, self.iterator())
+
+    def test_main(self):
+        self.run_test()
+        self.run_test('P1')
+        self.run_test('P101')
+        self.run_test('P101,P1')
+
+    def test_main_invalid(self):
+        self.assertRaises(SystemExit, flake8_indexed_format.main,
+            ['--ignore', 'foobar', '/dev/null'])
+
+
+class TestMainOutdated(TestPatchedPrint, TestCaseBase):
+
+    def setUp(self):
+        super(TestMainOutdated, self).setUp()
+        self._old_argparse = flake8_indexed_format.argparse
+        flake8_indexed_format.argparse = ImportError()
+
+    def tearDown(self):
+        flake8_indexed_format.argparse = self._old_argparse
+        super(TestMainOutdated, self).setUp()
+
+    def test_create_parser(self):
+        self.assertIs(flake8_indexed_format.create_parser(None, None), False)
+        self.assertEqual(self.messages,
+                         ['argparse is required for the standalone version.'])
+
+    def test_execute(self):
+        self.assertIs(flake8_indexed_format.execute(None, None, None), False)
+        self.assertEqual(self.messages,
+                         ['argparse is required for the standalone version.'])
+
+    def test_main(self):
+        self.assertIs(flake8_indexed_format.main([]), False)
+        self.assertEqual(self.messages,
+                         ['argparse is required for the standalone version.'])
 
 
 if __name__ == '__main__':
