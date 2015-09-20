84c348fe3be0104bbf992510dde79b9db0f227a1
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-09-20 20:31:17 +0000

    [FEAT] Do tests for str.format

--- test_flake8_string_format.py
+++ test_flake8_string_format.py
@@ -23,10 +23,17 @@ def generate_code():
     for variant in itertools.product(
             ['', '#', '    '], ['', 'u', 'b'], ['', '0', 'param'], ['', ':03'],
             ['', 'Before'], ['', 'After']):
+        variant = list(variant)
         indented = variant[0].startswith(' ')
-        for use_format in [False, True]:
-            if use_format:
-                fmt_code = '.format({0}42)'
+        for use_format in [0, 1, 2]:
+            offset = 0 if not indented else 4
+            if use_format > 0:
+                if use_format == 1:
+                    fmt_code = '.format({0}42)'
+                else:
+                    fmt_code = ', {0}42)'
+                    variant[0] += 'str.format('
+                    offset += 11
                 if variant[2] == 'param':
                     fmt_code = fmt_code.format('param=')
                 else:
@@ -37,7 +44,7 @@ def generate_code():
                 code += ['if True:']
             code += ['{0}{1}"{4}{{{2}{3}}}{5}"{fmt}'.format(*variant, fmt=fmt_code)]
             if not variant[2] and not variant[0].strip().startswith('#'):
-                positions += [(101 if use_format else 103, len(code), 0 if not indented else 4)]
+                positions += [(101 if use_format > 0 else 103, len(code), offset)]
     return '\n'.join(code), positions
 
 
